39ee8a1f74d577b0d7d60f3ffe41b9e06fd6e737
Przemyslaw Tyczynski <przemyslaw.tyczynski@allegro.pl>
2021-12-03 17:04:35 +0100

    Utils for tasks

---
+++ src/utils.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+
+function getData(folder, isTest = false) {
+  if (!folder) {
+    throw new Error('Folder is required to resolve input data.');
+  }
+
+  const inputFileName = isTest ? 'input.test.txt' : 'input.txt';
+
+  const data = fs
+    .readFileSync(path.resolve(__dirname, folder, inputFileName))
+    .toString();
+  const dataArray = data.split('\n').filter((record) => Boolean(record.trim()));
+
+  return dataArray;
+}
+
+module.exports = { getData };
